5080c08aaef1b221fe1f0099b1f984325a264b7c
KevinPinoDelSol <kevinpinodelsol@gmail.com>
2022-12-27 20:08:43 -0500

    login implementado

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 const express = require ('express');
-const app =express();
-var fs =require('fs');
-const indice = require ('./database/index.js');
+const app = express();
+var fs = require('fs');
+const indice = require ('./database.js');
+
 
 app.set('view', 'public');
+
 //#region rutas
 
-/*app.get('/:name', (req, res)=>{
-    let html = fs.readFileSync('./public/index.html', 'UTF-8');
-    //html=html.replace('<h1 align="left">KeterKnights</h1>', '<h1 align="left">KerKnights</h1>');
-    res.send(html);
-});*/
+app.use(express.json());
+app.use(indice);
 
 app.get('/', (req, res)=>{
     //let html = fs.readFileSync('./public/login.html', 'UTF-8');
     res.sendfile('./public/login.html');
 });
 
-app.post('/', (req,res)=>{
-    console.log('Usuario logeado');
-    console.log(req.body);
-        /*console.log(req.body.nombre);
-        indice.buscarNombre(req.body.nombre)
-        res.sendfile('index.html');*/
+app.get('/login', (req,res)=>{
+    let nombre = req.query.nombre;
+    console.log('Usuario logeado: '+ nombre);
+    console.log(res.indice);
+    res.indice.buscarNombre(nombre,(lugar)=>{
+        if(lugar==-1){
+            res.send('usuario no encontrado');
+        } else {
+            res.sendfile('./public/index.html');
+        }
+    });
+
 });
 
 app.use(express.static('public'));
